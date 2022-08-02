5bc010e6360db3e770f9abde93eaaed637914373
Teyhan USLU <teyhan.uslu@accenture.com>
2022-08-02 23:59:47 +0300

    Initial Commit

---
+++ Ogr_AVKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ogr_AVKit
+//
+//  Created by teyhan.uslu on 2.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
